d56343f6a3ccbd2c9a420cbd7a76936f6dbb8ee7
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2019-10-30 12:05:06 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup() { 
+  noCanvas();
+  var map = L.map('mapid').setView([30, 23.71622], 8);
+
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([37.97945, 23.71622]).addTo(map)
+    .bindPopup('ATENAS  <BR> 9*, 10, *11')
+    .openPopup();
+
+L.marker([37.44529, 25.32872]).addTo(map)
+    .bindPopup('MIKONOS<BR>DIAS 11*, 12, *13')
+    .openPopup();
+  
+L.marker([36.393154,25.461510]).addTo(map)
+    .bindPopup('SANTORINI<BR>DIAS 13*, 14, *15')
+    .openPopup();
+} 
+
+function draw() { 
+
+}
