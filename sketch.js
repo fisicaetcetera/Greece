577c8971c2c5c2bdb7893ab934ff139d557b70d5
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2019-10-30 14:17:56 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,15 +2,19 @@ function setup() {
   noCanvas();
   var map = L.map('mapid').setView([30, 23.71622], 8);
 
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-	L.marker([ -5.7853, -35.3288
+L.marker([ -5.7853, -35.3288
 ]).addTo(map)
     .bindPopup('SÃO GONÇALO  <BR> dia 1')
     .openPopup();
 	
+
+L.marker([45.81313, 15.97753]).addTo(map)
+    .bindPopup('ZAGREB  <BR> 2, 3, *4')
+    .openPopup();	
 
 L.marker([37.97945, 23.71622]).addTo(map)
     .bindPopup('ATENAS  <BR> 9*, 10, *11')
