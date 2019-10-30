70dce7c53f5691d557d0cceb51819d590388dfeb
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2019-10-30 14:39:12 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,16 +11,13 @@ L.marker([ -5.7853, -35.3288
     .bindPopup('SÃO GONÇALO  <BR> dia 1')
     .openPopup();
 	
-
 L.marker([45.81313, 15.97753]).addTo(map)
     .bindPopup('ZAGREB  <BR> 2, 3, *4<BR>
-	       Dia 2 - Zagreb
-•
+Dia 2 - Zagreb
 Chegada à bela capital da Croácia. Zagreb é a maior cidade do país, uma metrópole que soube
 conservar o espírito de épocas passadas e, ao mesmo tempo, incorporar as comodidades da
-vida moderna. Traslado para o hotel.
-Dia 3 – Zagreb
-•
+vida moderna. Traslado para o hotel.<br>
+Dia 3 – Zagreb<br>
 Café da manhã buffet. City Zagreb que está sentada sobre as cidades de Gradec e Kaptol.
 Gradec, ou Gornji Grad, antigo povoado real, foi fortificada contra os ataques dos tártaros no
 século XIII e ainda conserva algumas de suas muralhas e pórticos medievais. Em Kaptol, antigo
