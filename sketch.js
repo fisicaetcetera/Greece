7787fb7faabe248b151ce2ba0ef8a2c71a194c29
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2019-10-30 14:54:26 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,9 +13,7 @@ L.marker([ -5.7853, -35.3288
 	
 L.marker([45.81313, 15.97753]).addTo(map)
     .bindPopup('ZAGREB  <BR> 2, 3, *4<BR>
-	      Apreciaremos as belas igrejas e os
-palácios góticos e barrocos, como a Catedral de Santo Estevão, a Igreja de São Marcos e o
-Convento de Santa Clara, sede do Museu da cidade. Restante do dia livre.')
+	      Igrejas e palácios góticos e barrocos e museu da cidade. Restante do dia livre.')
     .openPopup();	
 	
 L.marker([43.508133, 16.440193]).addTo(map)
