e96d3565810c7dedef929873c6386d4f8507495b
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2020-01-09 14:20:42 -0300

    Update sketch.js
    Retirei Lest europeu
    Inclui Heraclio e cidades de trânsito,
    Fortaleza e Amsterdam.

--- sketch.js
+++ sketch.js
@@ -7,32 +7,35 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.marker([ -5.7853, -35.3288]).addTo(map)
-    .bindPopup('SÃO GONÇALO  <BR> dia 1')
+    .bindPopup('SÃO GONÇALO  <BR> dia 14 17:20')
     .openPopup();
 	
-L.marker([45.81313, 15.97753]).addTo(map)
-    .bindPopup('ZAGREB  <BR> 2, 3, *4<BR>')
+L.marker([ -3.71839, -38.5434]).addTo(map)
+    .bindPopup('FORTALEZA  <BR> dia 14 19:45 ')
     .openPopup();	
 	
-L.marker([43.508133, 16.440193]).addTo(map)
-    .bindPopup('SPLIT  <BR> 4*, 5, 6, *7')
-    .openPopup();	
-	
-L.marker([42.64807, 18.09216]).addTo(map)
-    .bindPopup('DUBROVNIK  <BR> 7*, 8, *9')
-    .openPopup();	
+L.marker([ 52.370216, 4.895168]).addTo(map)
+    .bindPopup('AMSTERDAN  <BR> dia 15 12:20')
+    .openPopup();		
 
 L.marker([37.97945, 23.71622]).addTo(map)
-    .bindPopup('ATENAS  <BR> 9*, 10, *11')
+    .bindPopup('ATENAS  <BR> dia 16: passeio pela manhã <BR>
+	       e tarde livre')
     .openPopup();
 
 L.marker([37.44529, 25.32872]).addTo(map)
-    .bindPopup('MIKONOS<BR>DIAS 11*, 12, *13')
+    .bindPopup('MIKONOS<BR> dias 18 e 19')
     .openPopup();
   
 L.marker([36.393154,25.461510]).addTo(map)
-    .bindPopup('SANTORINI<BR>DIAS 13*, 14, *15')
+    .bindPopup('SANTORINI<BR> dias 21 e 22')
     .openPopup();
+
+  L.marker([35.338735 , 25.144213]).addTo(map)
+    .bindPopup('HERAKLIO<BR> dia 24')
+    .openPopup();
+	
+	
 } 
 
 function draw() { 
