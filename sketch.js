1715d0ea1bae1c85836a844aae2bdeda28415cd0
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2020-01-09 21:13:55 -0300

    Update sketch.js
    Parece que view está errado.  Coloca emAtenas, aparece Heraklio

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 function setup() { 
   noCanvas();
-  var map = L.map('mapid').setView([37.97945, 23.71622], 18);
+  var map = L.map('mapid').setView([37.97945, 23.71622], 12);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
