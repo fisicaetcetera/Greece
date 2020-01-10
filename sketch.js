e490945733ffaeae8510afe942af13a9b7c2051f
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2020-01-09 21:24:53 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 function setup() { 
   noCanvas();
-  var map = L.map('mapid').setView([35, 25], 2);
+  var map = L.map('mapid').setView([35, 25], 4);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
