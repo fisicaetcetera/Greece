32edeb6f35aeac1430bd2ca2b21ba95c5491b4e9
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2020-01-12 06:54:18 -0300

    Add files via upload
    figura de acropolis e novo sketch

--- sketch.js
+++ sketch.js
@@ -1,12 +1,49 @@
+var imageUrl;
+let imagem;
+let parthenonlat = 37.97025; 
+let parthenonlong = 23.72247;
+let acropolislat  = 37.96855;
+let acropolislong = 23.72848;
+
+
+let opacity = 1.0;
+
 function setup() {
   noCanvas();
-  //var map = L.map('mapid').setView([35.338735 , 25.144213], 2);
-  var map = L.map('mapid').fitWorld();
+  var map = L.map('mapid').setView([37.97945, 23.71622], 2);
+ //Paternon 
+    var imageUrl = ['./parthenon.jpg'];
+
+    var bounds = L.latLngBounds([
+      [parthenonlat, parthenonlong],
+      [parthenonlat+0.01, parthenonlong+0.02]
+    ]);
+
+    var imageOverlay = L.imageOverlay( imageUrl, bounds, {
+        opacity: 0.3
+    }).addTo(map);
+
+ // Museu de Acrópolis
+  
+    var imageUrl = ['./acropolis.jpg'];
+
+    var bounds = L.latLngBounds([
+      [ acropolislat, acropolislong],
+      [ acropolislat-0.005, acropolislong-0.0052]
+    ]);
+
+    var imageOverlay = L.imageOverlay( imageUrl, bounds, {
+        opacity: 0.5
+    }).addTo(map);
+
+
+  
+  
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
-
-  L.marker([ -5.759290, -35.368370]).addTo(map)
+  
+  L.marker([-5.759290, -35.368370]).addTo(map)
     .bindPopup('AEROPORTO  <BR> dia 14 17:20')
     .openPopup();
 
@@ -21,6 +58,14 @@ function setup() {
   L.marker([37.97945, 23.71622]).addTo(map)
     .bindPopup('ATENAS  <BR> dia 16: passeio pela manhã <BR> e tarde livre')
     .openPopup();
+  
+   L.marker([parthenonlat, parthenonlong]).addTo(map)
+    .bindPopup('ACRÓPOLIS')
+    .openPopup();
+  
+   L.marker([ acropolislat, acropolislong]).addTo(map)
+    .bindPopup('MUSEU DE ACRÓPOLIS')
+    .openPopup();
 
   L.marker([37.44529, 25.32872]).addTo(map)
     .bindPopup('MIKONOS<BR> dias 18 e 19')
@@ -33,26 +78,5 @@ function setup() {
   L.marker([35.338735, 25.144213]).addTo(map)
     .bindPopup('HERAKLIO<BR> dia 24')
     .openPopup();
-
-  map.locate({
-    setView: true,
-    maxZoom: 12
-  });
-
-  function onLocationFound(e) {
-    var radius = e.accuracy;
-
-    L.marker(e.latlng).addTo(map)
-      .bindPopup("Você!").openPopup();
-
-    L.circle(e.latlng, radius).addTo(map);
-  }
-
-  map.on('locationfound', onLocationFound);
-
-
-}
-
-function draw() {
-
+ 
 }
