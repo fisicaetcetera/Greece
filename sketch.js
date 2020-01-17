b7fa67760617156801b2bf7dbe7bc8365704e653
enivaldo bonelli <45775341+fisicaetcetera@users.noreply.github.com>
2020-01-17 11:53:38 -0300

    Update sketch.js
    mexendono tamanho do icone

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ var icone = L.icon({
     iconUrl: './images/marker-icon.png',
     shadowUrl: './images/marker-shadow.png',
 
-    iconSize:     [38, 95], // size of the icon
+    iconSize:     [22, 85], // size of the icon
     shadowSize:   [50, 64], // size of the shadow
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     shadowAnchor: [4, 62],  // the same for the shadow
